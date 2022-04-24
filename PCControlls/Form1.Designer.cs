68237cb999196e99ee5faab4f53be23ef002052e
simonuks221 <simonasriauka@gmail.com>
2022-04-24 19:20:07 +0300

    PC controlls

---
+++ PCControlls/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace POV_Globe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.RotateRightButton = new System.Windows.Forms.Button();
+            this.RotateLeftButton = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(152, 28);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(7, 29);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(138, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(647, 16);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(254, 72);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Port";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.FullOpen = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.RotateRightButton);
+            this.groupBox3.Controls.Add(this.RotateLeftButton);
+            this.groupBox3.Location = new System.Drawing.Point(14, 159);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox3.Size = new System.Drawing.Size(887, 109);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Text";
+            // 
+            // RotateRightButton
+            // 
+            this.RotateRightButton.Location = new System.Drawing.Point(649, 41);
+            this.RotateRightButton.Name = "RotateRightButton";
+            this.RotateRightButton.Size = new System.Drawing.Size(87, 37);
+            this.RotateRightButton.TabIndex = 3;
+            this.RotateRightButton.Text = "Right";
+            this.RotateRightButton.UseVisualStyleBackColor = true;
+            this.RotateRightButton.Click += new System.EventHandler(this.RotateRightButton_Click);
+            // 
+            // RotateLeftButton
+            // 
+            this.RotateLeftButton.Location = new System.Drawing.Point(481, 38);
+            this.RotateLeftButton.Name = "RotateLeftButton";
+            this.RotateLeftButton.Size = new System.Drawing.Size(87, 41);
+            this.RotateLeftButton.TabIndex = 2;
+            this.RotateLeftButton.Text = "Left";
+            this.RotateLeftButton.UseVisualStyleBackColor = true;
+            this.RotateLeftButton.Click += new System.EventHandler(this.RotateLeftButton_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(7, 29);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(86, 31);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "START";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.button7);
+            this.groupBox5.Location = new System.Drawing.Point(538, 17);
+            this.groupBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox5.Size = new System.Drawing.Size(102, 71);
+            this.groupBox5.TabIndex = 8;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Motor";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(583, 286);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(94, 29);
+            this.StopButton.TabIndex = 9;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 460);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button RotateRightButton;
+        private System.Windows.Forms.Button RotateLeftButton;
+        private System.Windows.Forms.Button StopButton;
+    }
+}
+
---
+++ PCControlls/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.Threading;
+using System.IO.Ports;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+
+namespace POV_Globe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            getAvailableComPorts();
+        }
+
+        private String[] ports;
+        private SerialPort port;
+        private bool isConnected = false;
+        private bool motorOn = false;
+
+        void getAvailableComPorts()
+        {
+            ports = SerialPort.GetPortNames();
+            Debug.WriteLine("a");
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+                Debug.WriteLine("rado");
+            }
+            if (ports.Length != 0)
+            {
+                comboBox1.SelectedItem = ports[0];
+            }
+        }
+
+        private void connectController()
+        {
+            if (!isConnected)
+            {
+                string selectedPort = comboBox1.GetItemText(comboBox1.SelectedItem);
+                port = new SerialPort(selectedPort, 921600, Parity.None, 8, StopBits.One);
+                port.Open();
+                isConnected = true;
+                button1.Text = "Connected";
+            }
+        }
+
+        /*
+        private void sendPicture()
+        {
+            var Image = (Bitmap)pictureBox1.Image;
+
+            int ARRAY_SIZE = (144 * 72) * 3 + 1 + 1; // RGB values + start byte + end byte
+
+            byte[] inline = new byte[ARRAY_SIZE]; 
+
+            inline[0] = (byte)'I';
+
+            for (int i = 0; i < Image.Width; i++)
+            {
+                for (int j = 0; j < Image.Height; j++)
+                {
+                    inline[1 + (i * Image.Height * 3) + (j * 3) + 0] = Image.GetPixel(i, j).G;
+                    inline[1 + (i * Image.Height * 3) + (j * 3) + 1] = Image.GetPixel(i, j).B;
+                    inline[1 + (i * Image.Height * 3) + (j * 3) + 2] = Image.GetPixel(i, j).R;
+                }
+            }
+
+            inline[ARRAY_SIZE - 1] = (byte)'\n';
+
+            if (isConnected)
+            {
+                port.Write(inline, 0, ARRAY_SIZE);
+            }
+        }
+        */
+
+
+
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connectController();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            
+            int ARRAY_SIZE = 2; // RGB values + start byte + end byte
+
+            byte[] inline = new byte[ARRAY_SIZE];
+
+            if (motorOn)
+            {
+                inline[0] = (byte)'L';
+            }
+            else
+            {
+                inline[0] = (byte)'L';
+            }
+
+            inline[ARRAY_SIZE - 1] = (byte)'\n';
+
+            if (isConnected)
+            {
+                port.Write(inline, 0, ARRAY_SIZE);
+                Debug.WriteLine(inline[0]);
+                motorOn = !motorOn;
+                if (motorOn)
+                {
+                    button7.Text = "STOP";
+                }
+                else
+                {
+                    button7.Text = "START";
+                }
+            }
+        }
+
+        private void RotateRightButton_Click(object sender, EventArgs e)
+        {
+            int ARRAY_SIZE = 4;
+
+            byte[] inline = new byte[ARRAY_SIZE];
+
+            if (motorOn)
+            {
+                inline[0] = (byte)'R';
+                inline[1] = (byte)'\n';
+                inline[2] = (byte)'R';
+            }
+            else
+            {
+                inline[0] = (byte)'R';
+                inline[1] = (byte)'\n';
+                inline[2] = (byte)'R';
+            }
+
+            inline[ARRAY_SIZE - 1] = (byte)'\n';
+
+            if (isConnected)
+            {
+                port.Write(inline, 0, ARRAY_SIZE);
+            }
+        }
+
+        private void RotateLeftButton_Click(object sender, EventArgs e)
+        {
+            int ARRAY_SIZE = 4;
+
+            byte[] inline = new byte[ARRAY_SIZE];
+
+            if (motorOn)
+            {
+                inline[0] = (byte)'R';
+                inline[1] = (byte)'\n';
+                inline[2] = (byte)'L';
+            }
+            else
+            {
+                inline[0] = (byte)'R';
+                inline[1] = (byte)'\n';
+                inline[2] = (byte)'L';
+            }
+
+            inline[ARRAY_SIZE - 1] = (byte)'\n';
+
+            if (isConnected)
+            {
+                port.Write(inline, 0, ARRAY_SIZE);
+                Debug.WriteLine(inline[0]);
+                Debug.WriteLine(inline[1]);
+                Debug.WriteLine(inline[2]);
+            }
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            int ARRAY_SIZE = 2;
+
+            byte[] inline = new byte[ARRAY_SIZE];
+
+            if (motorOn)
+            {
+                inline[0] = (byte)'S';
+
+            }
+            else
+            {
+                inline[0] = (byte)'S';
+
+            }
+
+            inline[ARRAY_SIZE - 1] = (byte)'\n';
+
+            if (isConnected)
+            {
+                port.Write(inline, 0, ARRAY_SIZE);
+            }
+        }
+    }
+}
